b59213c768dc4e402ef53ab240ecabdf65f50a2f
KASHISH <kashu80525@gmail.com>
2023-04-22 19:33:43 +0530

    adding codes for adding two arrays in new array

--- block-BJaaco/code/index.js
+++ block-BJaaco/code/index.js
@@ -3,11 +3,22 @@ let numbers = [6, 8, 10, 12, 43, 56, 98];
 let userIds = [1230, 234, 1278, 984, 763, 900];
 
 // 1. Add all the values of numbers and userIds array into the new newly created array named `collection`
-
+let collection = [];
+collection = numbers.concat(userIds);
 // 2. Add all the even numbers from both arrays numbers and userIds into a newly created array named `evenCollection`
-
+let evenCollection = [];
+for (let collect of collection){
+  if ( collect % 2 == 0){
+    evenCollection.push(collect);
+  }
+}
 // 3. Add all the odd numbers from both arrays numbers and userIds into a newly created array named `oddCollection`
-
+let oddCollection = [];
+for (let collect of collection){
+  if ( collect % 2 != 0){
+    oddCollection.push(collect);
+  }
+}
 /*
   @param means parameter
 
@@ -24,8 +35,8 @@ let userIds = [1230, 234, 1278, 984, 763, 900];
     times(5); // ['test', 'test', 'test', 'test', 'test']
 */
 
-function times() {
-  // Your code
+function times(number , string) {
+  
 }
 
 // Uncomment the code below and test the output
