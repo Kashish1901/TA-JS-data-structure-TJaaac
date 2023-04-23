a27ce01c9f0f8e14aa744fdce3755cceec021bb3
KASHISH <kashu80525@gmail.com>
2023-04-23 07:15:17 +0530

    adding codes

--- block-BJaacr/code/index.js
+++ block-BJaacr/code/index.js
@@ -19,13 +19,15 @@ Hint: (Structure of got variable)
 */
 
 // 1. Log the value of key "houses"
-
+console.log(got.houses);
 // 2. Log the length of the "houses" key
-
+console.log(got.houses.length);
 // 3. Log the name of all houses in got one by one
-
+for (house of got.houses){
+  console.log(got.house)
+}
 // 4. Log the name of all the people in house Starks (index 0) one by one
-
+console.log(got.house[0].name)
 // 5. Log the name of all the people in house Lannisters (index 1) one by one
 
 // 6. Log the description of all the people in house Lannisters (index 1) one by one
